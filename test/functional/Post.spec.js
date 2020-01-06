9115b7f32311b07ed562d4e145cf32308abe199c
Carvalho, Vinicius Luiz <carvalho.viniciusluiz@gmail.com>
2020-01-06 11:28:16 -0300

    feat(test): Adding mutation

--- test/functional/Post.spec.js
+++ test/functional/Post.spec.js
@@ -23,7 +23,40 @@ test('get list of posts', async ({ client, assert }) => {
   const response = await client.post('/').send(data).end()
 
   response.assertStatus(200)
-  assert.exists(response.body.data)
   assert.exists(response.body.data.posts)
   assert.equal(response.body.data.posts.length, 9)
+})
+
+test('get one post', async ({ client, assert }) => {
+  const data = {
+    query: `
+      {
+        posts(id:9) {id}
+      }
+    `
+  }
+
+  const response = await client.post('/').send(data).end()
+
+  response.assertStatus(200)
+  assert.exists(response.body.data.posts)
+  assert.equal(response.body.data.posts.length, 1)
+})
+
+test('insert new post', async ({ client, assert }) => {
+  const data = {
+    query: `
+      mutation {
+        post: addPost(post:{title: "xxx", content: "yyy"}) {
+          id
+        }
+      }
+    `
+  }
+
+  const response = await client.post('/').send(data).end()
+
+  response.assertStatus(200)
+  assert.exists(response.body.data.post)
+  assert.exists(response.body.data.post.id)
 })
